59f8891a70673a60268c10b2dc4f9047d7129dbf
hYeongJunK1m <hyeongjun94@gmail.com>
2021-03-11 18:15:42 +0900

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+    public static void main(String[] args) {
+
+        int num;
+        int num1 = 1;
+        int num2 = 2;
+
+        num = num1 + num2;
+        System.out.println(num);
+
+        String name = "HyeongJun Kim";
+        int age = 28;
+
+        System.out.println("My name is " + name);
+        System.out.println("My age is " + age);
+
+    }
+}
