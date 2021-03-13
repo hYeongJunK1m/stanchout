cd3342f9b0a8f2838c3803c441656de6a2f8b6c0
hYeongJunK1m <hyeongjun94@gmail.com>
2021-03-13 23:55:02 +0900

    feat : 반복 연습

---
+++ src/H.java
@@ -0,0 +1,34 @@
+public class H {
+
+    public static void main(String[] args) {
+        System.out.print("아직 코딩 적응이");
+        System.out.print(" 안된것인가?");
+        System.out.println("=======================");
+
+        System.out.println("왜 자꾸 뒤돌아보면 까먹지?");
+        System.out.println("=======================");
+        int sum;
+        int a = 5;
+        int b = 10;
+
+        sum = a+b;
+
+        System.out.println(sum);
+    }
+    public int add(int c, int d){
+        c = 5;
+        d = 10;
+        int sum = c+d;
+        return sum;
+
+
+    }
+    public String str(){
+        String str;
+        str = "삐용삐용";
+
+        return str;
+    }
+
+}
+
