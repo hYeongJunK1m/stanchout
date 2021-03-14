9482cc184d65a7354dbc5b6cf2844f339e5b96c0
hYeongJunK1m <hyeongjun94@gmail.com>
2021-03-14 23:55:57 +0900

    feat : 접근자, 리턴타입, 메소드 이름 매개변수 정리

---
+++ src/PublicNote.java
@@ -0,0 +1,14 @@
+public class PublicNote {
+
+    //  public 접근제어자 / class 클래스 / 000 클래스 이름&파일명
+    public static void main(String[] args) {
+
+        //public 접근제어자 : 메소드는 접근 권한을 설정하기 위해 접근제어자를 설정해야한다. (public, private, protected 등이 있다.)
+        // public은 다른 패키지에서도 객체 생성이 가능, 다른 클래스에서 new를 통해 호출할 수 있다.
+        // static : 공유한다는 의미
+        // void : return 타입이 없음을 의미
+        //main() : 자바 응용프로그램 실행은 main() 메소드로부터 시작한다.
+
+
+        }
+}
